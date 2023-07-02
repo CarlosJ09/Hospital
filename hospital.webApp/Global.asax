10acc7fd63669b17e62bff4a9fbc1c9e73d34522
Donato Machado Santos <donato.machadosantos@gmail.com>
2023-07-02 11:36:28 -0400

    Creacion del WebApp + algunos detalles en la web

---
+++ hospital.webApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="hospital.webApp.MvcApplication" Language="C#" %>
---
+++ hospital.webApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(hospital.webApp.Startup))]
+namespace hospital.webApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
