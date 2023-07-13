32ab3c281f657fb5f57a9339295c7399b93e4010
Carlos <carlos.ml.jimenezz@gmail.com>
2023-07-13 10:48:04 -0400

    Creacion de Controladores (Integracion)

---
+++ hospital.integracion/Controllers/CajaController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace hospital.integracion.Controllers
+{
+    [ApiController]
+    [Produces("Application/Json")]
+    [Route("api/[controller]")]
+    public class CajaController : Controller
+    {
+        [HttpGet("AllClients")]
+        public async Task<IActionResult> AllClients()
+        {
+            var response = "Clients";
+            return Ok(response);
+        }
+
+        [HttpGet("AllUsuarios")]
+        public async Task<IActionResult> AllUsuarios()
+        {
+            var response = "Usuarios";
+            return Ok(response);
+        }
+    }
+}
---
+++ hospital.integracion/Controllers/CoreController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace hospital.integracion.Controllers
+{
+    [ApiController]
+    [Produces("Application/Json")]
+    [Route("api/[controller]")]
+    public class CoreController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
